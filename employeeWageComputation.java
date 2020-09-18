57f4b1d990175ba5425a944af70f8496550af5f1
SAHITHYA1998 <sahithyahi@gmail.com>
2020-09-18 21:56:46 +0530

    added : Employee Part Time Daily Wage Added

--- employeeWageComputation.java
+++ employeeWageComputation.java
@@ -1,22 +1,32 @@
 public class EmpWageComputation
 {
         public static final int WAGE_PER_HR=20;
+        public static final int PART_TIME=2;
+        public static final int FULL_TIME=1;
+
+
         public static void main(String[] args)
         {
-                int IS_FULL_TIME=1;
                 int salary,workingHr=0;
-                double empCheck=Math.floor(Math.random()*10)%2;
-                if (empCheck==IS_FULL_TIME)
+                int random=(int)Math.floor(Math.random()*10)%3;
+                System.out.println(random);
+
+                if(FULL_TIME==random)
                 {
-                        System.out.println("Employee is Present");
+                        System.out.println("Full time Employee");
                         workingHr=8;
+                }
+                else if(PART_TIME==random)
+                {
+
+                        System.out.println("Part time Employee");
+                        workingHr=4;
                 }
                 else
                 {
-                        System.out.println("Employee is Absent");
+                        System.out.println("Employee is absent");
                 }
                 salary=workingHr*WAGE_PER_HR;
                 System.out.println("Employee Wage: "+salary);
         }
-
 }
