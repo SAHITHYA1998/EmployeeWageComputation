cca9238cb634195124ba0bbe4ea507dffab42f49
SAHITHYA1998 <sahithyahi@gmail.com>
2020-09-18 22:20:43 +0530

    Refactor : Class Method Added

--- employeeWageComputation.java
+++ employeeWageComputation.java
@@ -1,19 +1,19 @@
-public class EmpWageWhileLoop
+import java.util.Scanner;
+
+public class EmpWageClassMethod
 {
 
-                public static final int WAGE_PER_HR=20;
                 public static final int FULL_TIME=1;
                 public static final int PART_TIME=0;
-                public static final int NUMBER_OF_WORKING_DAYS=20;
-                public static final int NUMBER_OF_HOURS=100;
-                public static void main(String[] args)
-                {
-                        int currentWorkingHour=0;
+						
+					public static void calculateEmpWage(String company,int wagePerHr,int numbOfWorkingDays,int numbWorkingHrs)
+					{
+						 int currentWorkingHour=0;
                         int currentWorkingday=0;
                         int salary,workingHr=0;
                         int totalSalary=0;
 
-                        while( NUMBER_OF_WORKING_DAYS>=currentWorkingday && NUMBER_OF_HOURS>=currentWorkingHour )
+                        while( numbOfWorkingDays>=currentWorkingday && numbWorkingHrs>=currentWorkingHour )
                         {
                                 int random=(int)Math.floor(Math.random()*10)%3;
                                 switch(random)
@@ -29,20 +29,26 @@ public class EmpWageWhileLoop
                                         default:
                                                 workingHr=0;
                                  }
-                                        if( currentWorkingHour!=96 )
-                                        {
-                                                currentWorkingHour+=workingHr;
-                                        }
-                                        else
-                                        {
-                                                currentWorkingHour=currentWorking+4;
-                                                salary=4*WAGE_PER_HR;
-                                        }
                                         currentWorkingday++;
-                                        salary=workingHr*WAGE_PER_HR;
+                                        salary=workingHr*wagePerHr;
                                          totalSalary+=salary;
                                         //System.out.println("Wage: "+salary);
                         }
-                        System.out.println(totalSalary);
+                        System.out.println("Total computed salary for "+company+" "+totalSalary);
+
+					}
+               
+					 public static void main(String[] args)
+                {
+							System.out.println("please enter company name:");
+							Scanner sc=new Scanner(System.in);
+							String company=sc.nextLine();
+
+							System.out.println("please enter numberofworkingdays:");
+                     int numbOfWorkingDays=sc.nextInt();
+
+							calculateEmpWage(company,numbOfWorkingDays,20,30);
+							calculateEmpWage("Microsoft",20,20,30);
+							
                 }
 }
