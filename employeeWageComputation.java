c5152f188ad9cbb7e819c8cfb21e4327b31e3d15
SAHITHYA1998 <sahithyahi@gmail.com>
2020-09-18 22:28:29 +0530

    added : Multiple Company Wage Calculated

--- employeeWageComputation.java
+++ employeeWageComputation.java
@@ -1,19 +1,32 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
-public class EmpWageClassMethod
+public class EmpWageObjectOriented
 {
 
                 public static final int FULL_TIME=1;
                 public static final int PART_TIME=0;
-						
-					public static void calculateEmpWage(String company,int wagePerHr,int numbOfWorkingDays,int numbWorkingHrs)
-					{
-						 int currentWorkingHour=0;
+			
+		public final String company;
+		public final int wagePerHr;
+		public final int numbOfWorkingDays;
+		public final int numbWorkingHrs;	
+		
+		public EmpWageObjectOriented(String company,int wagePerHr,int numbOfWorkingDays,int numbWorkingHrs)
+		{
+			this.company=company;
+			this.wagePerHr=wagePerHr;
+			this.numbOfWorkingDays=numbOfWorkingDays;
+			this.numbWorkingHrs=numbWorkingHrs;
+		}
+		public static void calculateEmpWage(EmpWageObjectOriented company)
+		{
+			int currentWorkingHour=0;
                         int currentWorkingday=0;
                         int salary,workingHr=0;
                         int totalSalary=0;
 
-                        while( numbOfWorkingDays>=currentWorkingday && numbWorkingHrs>=currentWorkingHour )
+                        while( company.numbOfWorkingDays>=currentWorkingday && company.numbWorkingHrs>=currentWorkingHour )
                         {
                                 int random=(int)Math.floor(Math.random()*10)%3;
                                 switch(random)
@@ -30,25 +43,46 @@ public class EmpWageClassMethod
                                                 workingHr=0;
                                  }
                                         currentWorkingday++;
-                                        salary=workingHr*wagePerHr;
+                                        salary=workingHr*company.wagePerHr;
                                          totalSalary+=salary;
                                         //System.out.println("Wage: "+salary);
                         }
-                        System.out.println("Total computed salary for "+company+" "+totalSalary);
+                        System.out.println("Total computed salary for "+company.company+" "+totalSalary);
 
 					}
-               
-					 public static void main(String[] args)
-                {
-							System.out.println("please enter company name:");
-							Scanner sc=new Scanner(System.in);
-							String company=sc.nextLine();
 
-							System.out.println("please enter numberofworkingdays:");
-                     int numbOfWorkingDays=sc.nextInt();
+        public static ArrayList readInputsFromUser()
+	{
+		ArrayList companyDetails = new ArrayList();
+		System.out.println("please enter number of companies");
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
 
-							calculateEmpWage(company,numbOfWorkingDays,20,30);
-							calculateEmpWage("Microsoft",20,20,30);
-							
+		for(int i=0;i<n;i++)
+		{
+			Scanner sc1=new Scanner(System.in);
+                	System.out.println("please enter company name:");
+                	String company=sc1.nextLine();
+                	companyDetails.add(company);
+                	System.out.println("please enter numberofworkingdays:");
+                	int numbOfWorkingDays=sc1.nextInt();
+                	companyDetails.add(numbOfWorkingDays);
+
+		}
+		return companyDetails;
+	}       
+	public static void main(String[] args)
+       	{
+		
+		ArrayList array=readInputsFromUser();
+		int days=(int)array.get(1);
+		int day=(int)array.get(3);
+
+
+		EmpWageObjectOriented object=new EmpWageObjectOriented(array.get(0).toString(),days,20,30);
+		object.calculateEmpWage(object);
+		EmpWageObjectOriented MicrosoftObject=new EmpWageObjectOriented(array.get(2).toString(),day,34,23);
+
+                MicrosoftObject.calculateEmpWage(MicrosoftObject);					
                 }
 }
