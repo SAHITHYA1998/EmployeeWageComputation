844a30602b179e6d673b6ea606a5cacb95acafb1
SAHITHYA1998 <sahithyahi@gmail.com>
2020-09-18 22:06:44 +0530

    added : Monthly Wage Calculated

--- employeeWageComputation.java
+++ employeeWageComputation.java
@@ -1,27 +1,41 @@
-public class EmployeeWage
-{
-        public static final int WAGE_PER_HR=20;
-        public static final int FULL_TIME=1;
-        public static final int PART_TIME=0;
-        public static void main(String[] args)
-        {
-                int salary,workingHr=0;
-                int random=(int)Math.floor(Math.random()*10)%3;
-                System.out.println(random);
-                switch(random){
-                case FULL_TIME:
-                        System.out.println("employee is present");
-                        workingHr=8;
-                        break;
-                case PART_TIME:
-                        System.out.println("Part Time");
-                        workingHr=4;
-                        break;
-                default:
-                        System.out.println("Absent");
-                        workingHr=0;
-                }
-                salary=workingHr*WAGE_PER_HR;
-                System.out.println("Wage: "+salary);
-        }
+public class EmpWage2 {
+
+	        public static final int WAGE_PER_HR=20;
+	        public static final int FULL_TIME=1;
+	        public static final int PART_TIME=0;
+	        public static final int NUMBER_OF_WORKING_DAYS=20;
+	        
+	        
+	        public static void main(String[] args)
+	        
+	        {
+	                int salary,workingHr=0;
+	                int totalSalary=0;
+	                for(int day=0;day<NUMBER_OF_WORKING_DAYS ;day++) {
+	                int random=(int)Math.floor(Math.random()*10)%3;
+	                System.out.println(random);
+	                switch(random)
+	                {
+	                case FULL_TIME:
+	                        System.out.println("employee is present");
+	                        workingHr=8;
+	                        break;
+	                case PART_TIME:
+	                        System.out.println("Part Time");
+	                        workingHr=4;
+	                        break;
+	                default:
+	                        System.out.println("Absent");
+	                        workingHr=0;
+	                }
+	                
+	                salary=workingHr*WAGE_PER_HR;
+	                 totalSalary+=salary;
+	                System.out.println("Wage: "+salary);
+	                }
+	                System.out.println(totalSalary);
+	        }
+	
+
 }
+  
